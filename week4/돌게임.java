b2e6c1a30e936f3d62c4da3d946923ddf293a844
Hanbee Lee <bee1162@naver.com>
2024-08-01 01:19:23 +0900

    week4-2: 수요일 문제

---
+++ week4/돌게임.java
@@ -0,0 +1,24 @@
+package week4;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class 돌게임 {
+    public static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+
+        if (N % 2 == 0) {
+            System.out.println("CY");
+        } else if ( N%2 == 1) {
+            System.out.println("SK");
+        }
+
+
+    }
+}
