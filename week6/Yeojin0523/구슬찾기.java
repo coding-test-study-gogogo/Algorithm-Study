84ce13749b196df9bf3c7ed31b9700bd4dd7100d
Yeojin0523 <d02jyjeong@naver.com>
2024-08-15 18:24:48 +0900

    수요일 추가

--- week6/Yeojin0523/구슬찾기.java
+++ week6/Yeojin0523/구슬찾기.java
@@ -1,27 +1,65 @@
 package week6;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 public class 구슬찾기 {
 
 	public static int N;
 	public static int M;
-	public static int[][] array;
+	
+	public static List<Integer>[] heavier, lighter;
+	public static boolean[] visited;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		N = sc.nextInt();
-		M = sc.nextInt();
+		N = sc.nextInt(); // 구슬 갯수
+		M = sc.nextInt(); // 비교 정보
 		
-		array = new int[M][2];
+		heavier = new ArrayList[N + 1];
+		lighter = new ArrayList[N + 1];
 		
-		for(int i = 0; i < array.length; i++) {
-			array[i][0] = sc.nextInt();
-			array[i][1] = sc.nextInt();
+		for(int i = 1; i <= N; i++) {
+			heavier[i] = new ArrayList<>();
+			lighter[i] = new ArrayList<>();
 		}
 		
-		sc.close();
-	}
-	
-	
+		for(int i = 0; i < M; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			heavier[b].add(a);
+			lighter[a].add(b);
+		}
+		int answer = 0;
+		int half = (N + 1) / 2;
 		
-
+		for(int i = 1; i <= N; i++) {
+			if(bfs(i, heavier) >= half || bfs(i, lighter) >= half) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
+		sc.close();
+	} 
+	public static int bfs(int start, List<Integer>[] graph) {
+		Queue<Integer> queue = new LinkedList<>();
+		visited = new boolean[N + 1];
+		queue.add(start);
+		visited[start] = true;
+		int count = 0;
+		
+		while(!queue.isEmpty()) {
+			int current = queue.poll();
+			for(int next : graph[current]) {
+				if(!visited[next]) {
+					visited[next] = true;
+					queue.add(next);
+					count++;
+				}
+			}
+		}
+		return count;
+	}
 }
