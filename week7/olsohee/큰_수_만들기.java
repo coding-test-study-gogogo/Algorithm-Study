cd6e3f2d0debae8e56a37243b8ecec2991811a07
olsohee <dlthgml0108@daum.net>
2024-08-23 13:05:21 +0900

    week7-5_olsohee

---
+++ week7/olsohee/큰_수_만들기.java
@@ -0,0 +1,40 @@
+package week7.olsohee;
+
+import java.util.*;
+
+class 큰_수_만들기 {
+
+    public String solution(String number, int k) {
+        int n = number.length();
+        int[] arr = Arrays.stream(number.split(""))
+                .mapToInt(str -> Integer.parseInt(str))
+                .toArray();
+
+        String answer = "";
+        int start = 0;
+
+        while (answer.length() < n - k) {
+
+            if (k == 0) {
+                for (int i = start; i < n; i++) {
+                    answer += arr[i];
+                }
+                break;
+            }
+
+            int max = arr[start];
+            int maxIdx = start;
+            for (int i = start; i <= start + k; i++) {
+                if (max < arr[i]) {
+                    max = arr[i];
+                    maxIdx = i;
+                    if (max == 9) break;
+                }
+            }
+            answer += max;
+            k -= maxIdx - start;
+            start = maxIdx + 1;
+        }
+        return answer;
+    }
+}
