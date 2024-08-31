e2b918306a05e935d8536258ab684855c99a3a2c
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-09-01 00:39:25 +0900

    week8-6_devzoe

---
+++ week8/devzoe/롤케이크_자르기.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func solution(_ topping: [Int]) -> Int {
+    var dict = [Int: Int]()
+    var setDict = Set<Int>()
+    var result = 0
+    
+    for t in topping {
+        dict[t, default: 0] += 1
+    }
+    
+    for i in topping {
+        if let count = dict[i] {
+            if count == 1 {
+                dict.removeValue(forKey: i)
+            } else {
+                dict[i] = count - 1
+            }
+        }
+        
+        setDict.insert(i)
+        
+        if dict.count == setDict.count {
+            result += 1
+        }
+    }
+    
+    return result
+}
